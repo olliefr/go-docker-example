390a7e74e34c499671f0aa5fb51d9c6fcdf06e4a
Oliver Frolovs <oliver@frolovs.me>
2021-02-19 07:41:22 +0200

    Update example to use CockroachDB

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module go-docker
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17
+require (
+	github.com/cockroachdb/cockroach-go v2.0.1+incompatible
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/lib/pq v1.4.0
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+
+	"context"
+	"database/sql"
+	"log"
+
+	"github.com/cockroachdb/cockroach-go/crdb"
+	_ "github.com/lib/pq"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -13,38 +22,102 @@ type Entry struct {
 	Value string `json:"value"`
 }
 
-// store is an ordered list of key-value pairs
-var store = make([]*Entry, 0)
-
 func main() {
 
+	// Read the connection parameters from environment variables,
+	// following the Twelve-Factor App philosophy: https://12factor.net/
+	username := os.Getenv("PGUSER")
+	password := os.Getenv("PGPASSWORD")
+	hostname := os.Getenv("PGHOST")
+	port := os.Getenv("PGPORT")
+	database := os.Getenv("PGDATABASE")
+
+	// Build the connection string
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		username, password, hostname, port, database)
+
+	// Initialise the store
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	defer db.Close()
+
+	// Create the table to store key-value pairs
+	if _, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS store (key STRING PRIMARY KEY, value STRING)"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialise the router
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// Configure the routes
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
+	e.GET("/list", func(c echo.Context) error {
+		return listEntries(db, c)
+	})
+	e.POST("/add", func(c echo.Context) error {
+		return addEntry(db, c)
+	})
 
-	e.GET("/list", listEntries)
-	e.POST("/add", addEntry)
-
-	e.Logger.Fatal(e.Start(":8000"))
+	// Blast off!
+	port = os.Getenv("GODOCKER_PORT")
+	if port == "" {
+		port = "8000"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
 // listEntries returns a full copy of the store in JSON format to the client.
-func listEntries(c echo.Context) error {
+func listEntries(db *sql.DB, c echo.Context) error {
+
+	rows, err := db.Query("SELECT key, value FROM store")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Temporary storage for the values read from the DB
+	store := map[string]string{}
+
+	for rows.Next() {
+		var key, value string
+		if err := rows.Scan(&key, &value); err != nil {
+			log.Fatal(err)
+		}
+		store[key] = value
+	}
+
 	return c.JSON(http.StatusOK, store)
 }
 
 // addEntry adds a new entry to the key-value store
 // and returns the value in JSON format to the client.
-func addEntry(c echo.Context) error {
+func addEntry(db *sql.DB, c echo.Context) error {
+
 	e := new(Entry)
 	if err := c.Bind(e); err != nil {
 		return err
 	}
-	store = append(store, e)
+
+	err := crdb.ExecuteTx(context.Background(), db, nil, func(tx *sql.Tx) error {
+		if _, err := tx.Exec(
+			"INSERT INTO store (key, value) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET value = excluded.value",
+			e.Key, e.Value); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
 	return c.JSON(http.StatusOK, e)
 }
